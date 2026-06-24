98226912d41c9fcd79965da678bd726e955e384e
karthikmshetty <karthik.m.shetty@7edge.com>
2026-06-24 06:52:13 +0530

    callback

---
+++ scripts/Concepts/4.Functions/2.ArrowFun.js
@@ -0,0 +1,13 @@
+// Arrow functions , here we No need to write name of the function
+
+let sum = (a,b)=>{
+    return a+b;
+}
+console.log(sum(2,2));
+
+
+
+// We can also remove the braces if there is only one statement
+
+let sub = (a,b)=>a-b;
+console.log(sub(3,2));
