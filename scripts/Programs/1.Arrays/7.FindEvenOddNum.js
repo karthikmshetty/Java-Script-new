f0b6341a7f1b5b0409f0abbc17d0d56c5836b447
karthikmshetty <karthik.m.shetty@7edge.com>
2026-06-29 09:26:33 +0530

    msg

--- scripts/Programs/1.Arrays/7.FindEvenOddNum.js
+++ scripts/Programs/1.Arrays/7.FindEvenOddNum.js
@@ -1,4 +1,4 @@
-
+// Solution 1 : Display seperatly
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const even = numbers.filter((num) =>{
@@ -11,3 +11,16 @@ const odd = numbers.filter((num)=>{
 
 console.log("Even Numbers",even);
 console.log("Odd Numbers",odd);
+
+
+
+// Solution 2 : Using object , if we want in optimised way
+const numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const result = {
+  evens: numbersArr.filter(num => num % 2 === 0),
+  odds:  numbersArr.filter(num => num % 2 !== 0)
+};
+
+console.log(result.evens); // [2, 4, 6, 8, 10]
+console.log(result.odds);  // [1, 3, 5, 7, 9]
